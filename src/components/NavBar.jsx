5c0fc0bcfab5f41dd7a1e956e2c909464e259536
Raemay <[melaniebelliard@outlook.fr](mailto:melaniebelliard@outlook.fr)>
2023-03-29 15:15:19 +0200

    added pokemon buttons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 
 export default function NavBar({pokemonIndex, setPokemonIndex, pokemonList}) {
-    const handleClick = () => {
-        setPokemonIndex(pokemonIndex +1)
-      }
   
-      const handlePreviousClick = () => {
-        setPokemonIndex(pokemonIndex -1)
-      }
-      
   return (
     <div>
-    {pokemonIndex > 0 ? <button onClick={handlePreviousClick}>Précédent</button> : null}
-    {pokemonIndex < pokemonList.length - 1 ? <button onClick={handleClick}>Suivant</button> : null}
+     {pokemonList.map((pokemon, index) => (
+          <ul key={index}>
+            <button onClick={() => setPokemonIndex(index)}>{pokemon.name}</button>
+          </ul>
+        ))}
     </div>
   )
 }
